798ce09f8457648841849381581621eb8f29ef06
liushijie <597270955@qq.com>
2021-05-24 17:32:21 +0800

    test1

---
+++ src/main/java/com/sekai/crm/utils/UUIDUtil.java
@@ -0,0 +1,13 @@
+package com.sekai.crm.utils;
+
+import java.util.UUID;
+
+public class UUIDUtil {
+	
+	public static String getUUID(){
+		
+		return UUID.randomUUID().toString().replaceAll("-","");
+		
+	}
+	
+}
